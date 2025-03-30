d90f04795c99aa09cedcdc62f8341e9891090471
MohammedSalmaan <mohammed.salmaan@bragg.group>
2025-03-30 16:10:26 +0530

    Featch and display customer, product and order details into template and alos configured url routings

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/', views.customer)
+    path('', views.home, name='home'),
+    path('products/', views.products, name='product'),
+    path('customer/<str:id>', views.customer, name='customer')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -27,5 +27,12 @@ def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products' : products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, id):
+    customer = Customer.objects.get(id=id)
+    orders = customer.order_set.all()
+    context = {
+        'customer': customer,
+        'orders': orders,
+        'total_orders': orders.count()
+    }
+    return render(request, 'accounts/customer.html', context)
